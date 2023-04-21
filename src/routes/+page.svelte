45a7f11d13c159ffe6590fc909a70f47e5cef7f7
John Hall <johngradyh@gmail.com>
2023-04-21 18:26:05 -0400

    started page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let image;
+
+  $: if (image) {
+    console.log(image);
+    
+    for (const imgData of image) {
+      console.log(`${imgData.name} has size ${imgData.size} and type ${imgData.type}`)
+    }
+  }
+</script>
+
+<style>
+  .view {
+    width: '100vw';
+    height: '100vh';
+    display: 'flex';
+    flex-direction: 'column';
+    justify-content: 'center';
+    align-items: 'flex-start';
+  }
+
+  h1 {
+    font-size: 3rem;
+    text-align: center;
+  }
+</style>
+
+<div class="view">
+  <h1>Vector Image Search</h1>
+  <label for="image-upload">Upload an image</label>
+  <input 
+    type="file" 
+    id="image-upload" 
+    accept="image/*" 
+    bind:files={image}
+  />
+</div>
+
+
